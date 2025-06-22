3c5b3075c12f34f25087f72bbf32e08f6481aa5d
BuildOnCloud <smithzgg@amazon.com>
2025-06-22 09:29:23 -0500

    Create claude4-DeepResearch-strands-api-workflow.py

---
+++ DeepResearch/mcp-clients/Claude4/claude4-DeepResearch-strands-api-workflow.py
@@ -0,0 +1,206 @@
+#File: claude4-DeepResearch-strands-api-workflow.py
+#Author: Chris Smith
+#Email: smithzgg@amazon.com
+#Created: 06/15/2025
+#Last Modified: 06/18/2025
+
+#Description:
+#    Deep Research MCP client using Claude4 built on the Strands
+#    framework using the Anthropic API.  This example is shows the typical multi-turn
+#    processing utilized for most Deep Research model orchestraion.  
+#    It uses AWS guardrails to allow for custom restrictions
+#    to the model behavoir if needed.
+
+#Usage:
+#    \$ python <filename>.py 
+    
+#Dependencies:
+#    - mcp
+#    - strands
+#    - boto3
+
+from mcp.client.streamable_http import streamablehttp_client
+from strands import Agent, tool
+from strands.models.anthropic import AnthropicModel
+from strands.tools.mcp.mcp_client import MCPClient
+import logging
+import boto3
+
+DEBUG = 0
+#set this variable to the number of deep research questions you want to generate for the research topic
+#more questions = more depth, but more processing time and context
+#default is 3
+NUM_QUESTIONS = "3"
+#set to true if you want to include searching internal AWS Knowledge Bases
+INTERNAL_SEARCH = "false"
+#set to true if you want to use custom AWS Guardrails
+USE_GUARDRAILS = "false"
+
+# Replace with your actual guardrail ID and version
+guardrail_id = "<Your Guardrails ID here>"
+guardrail_version = "Your Guardrails version here>"
+bedrock_runtime = boto3.client("bedrock-runtime", region_name="us-west-2")
+
+# Enables Strands debug log level
+logging.getLogger("strands").setLevel(logging.ERROR)
+
+# Sets the logging format and streams logs to stderr
+logging.basicConfig(
+    format="%(levelname)s | %(name)s | %(message)s",
+    handlers=[logging.StreamHandler()]
+)
+
+model = AnthropicModel(
+    client_args={
+        "api_key": "<Your Anthropic API Key here>",
+    },
+    # **model_config
+    max_tokens=8196,
+ #   model_id="claude-3-7-sonnet-20250219",
+    model_id="claude-sonnet-4-20250514",
+    params={
+        "temperature": 0.7,
+    }
+)
+
+# Use this if you are running the MCP server on the same system, otherwise replace localhost 
+# with the IP address of the MCP Server
+
+def create_streamable_http_transport():
+   return streamablehttp_client("http://localhost:8000/mcp/")
+
+# function to apply Bedrock Guardrails.  
+# Insert any custom Python code for additional guardrails
+
+def apply_bedrock_guardrail(input_text, source):
+    
+    gr_response = bedrock_runtime.apply_guardrail(
+        guardrailIdentifier=guardrail_id,
+        guardrailVersion=guardrail_version,
+        source=source,  
+        content=[{"text": {"text": input_text}}]
+    )
+    return gr_response
+
+def guardrail_check(input: str) -> str:
+     #check to ensure the model's response do not violate any of our guardrails
+    guardrail_response = apply_bedrock_guardrail(input, "OUTPUT")
+    #if they do, just do not record the response and continue to research
+    if guardrail_response["action"] == "GUARDRAIL_INTERVENED":
+            gr_response="This topic has triggered a guardrail and may not contain a complete response"
+            print("Guardrail intervened on the response:", guardrail_response["outputs"])
+    else:
+        gr_response=input
+    return gr_response
+
+def strands_turn(query: str, text: str) -> str:
+    if DEBUG:
+        print("Prompt: " + query)
+
+    if (USE_GUARDRAILS.lower() == "true") and (apply_bedrock_guardrail(str(query), "INPUT") == "GUARDRAIL_INTERVENED"):
+            print("Guardrail intervened on the response:", guardrail_response["outputs"])
+            text = text + "I am sorry due to content restrictions, I cannot process this request"
+    else:     
+        response = agent(query)
+        if DEBUG:
+            print("Response: " + str(response))
+            #check to ensure the model's response do not violate any of our guardrails
+        if USE_GUARDRAILS.lower()=="true" :
+            text=text + guardrail_check(str(response))
+        else:
+            text = text + str(response)
+    return text  
+
+
+streamable_http_mcp_client = MCPClient(create_streamable_http_transport)
+
+# Use the MCP server in a context manager
+#start MCP Client
+
+streamable_http_mcp_client = MCPClient(create_streamable_http_transport)
+
+with streamable_http_mcp_client:
+    #Get Tool List from MCP Server
+    tools = streamable_http_mcp_client.list_tools_sync()
+    #Initialize STrands Agent
+    #callback_handler = None for silent mode
+    agent = Agent(model=model, tools=tools, callback_handler=None, system_prompt="You are a deep research assistant")
+
+    #User input loop
+    while True:
+        #get User Input
+        query = input("\nQuery: ").strip()
+        # type 'quit' to exit loop gracefully
+        if query.lower() == 'quit':
+            break
+        # Step 1 - Generate 3 good deep research questions from the prompt        
+        question_prompt = "Generate " + NUM_QUESTIONS + " deep research questions from the following prompt separate each question with the | symbol. Respond with only the questions:" + query
+        #Check to ensure the user prompt does not violate our internal rules
+        if USE_GUARDRAILS.lower() == "true" :
+            guardrail_response = apply_bedrock_guardrail(str(question_prompt), "INPUT")
+        #if it does, just abort and prompt for the next question
+        if USE_GUARDRAILS.lower() == "true" and guardrail_response["action"] == "GUARDRAIL_INTERVENED":
+            print("Guardrail intervened on the response:", guardrail_response["outputs"])
+            print("I am sorry due to content restrictions, I cannot process this request")
+        else:
+            # Start Research
+            if DEBUG:
+                print("Prompt: "+ question_prompt)
+            
+            response1 = agent(question_prompt)
+
+            if DEBUG:
+                print("Response: " + str(response1))
+            questions_str = str(response1)
+            questions_list = questions_str.split("|")
+
+            #format final context
+            full_text = "<Content>"
+            #lets do a web search on each question
+            # separately and collate the responses
+            for question in questions_list :
+                full_text = strands_turn("Perform a web search for the following question:" + question, full_text)
+               
+            # For Deep research, we should also search ArXiv to see what recent papers have been published on
+            # this topic
+        
+            full_text = strands_turn("Perform an arXiv search on the following subject: " + query, full_text)
+            
+
+            #lets also check the historical stock performance if applicable
+            full_text = strands_turn("If the following prompt contains a company name, find the stock ticker for that company and get stock info for it: " + query, full_text)
+            
+
+            #we can get detailed company information and recent news 
+            full_text = strands_turn("If the following prompt contains a company name, find the stock ticker for that company and get financial news for it: " + query, full_text)
+            
+             # Lets make sure to search our internal data sources also
+            if INTERNAL_SEARCH.lower() == "true" : 
+                full_text = strands_turn("Search internal data sources for information on " + query, full_text)
+            
+
+            full_text=full_text + "</Content>"
+
+        
+            #We take all the collated information and analzye, summarize and format a final report.
+            #This section can be modified to create any desired report format
+        
+
+            query="From <Content> generate a plan to write a detailed 1500 word report on this topic: " + full_text
+            if (DEBUG):
+                print("Prompt: "+ query)
+            plan = agent(query)
+            if (DEBUG):
+                print("Response: " + str(plan))
+            plan = "<plan>" + str(plan) + "</plan>"
+            final_query = "Execute this [plan] to generate a 1500 word report with the following sections 1/Executive Summary 2/Detailed Analysis, Supporting data, and 3/ Reference links to answer this question: " + query + " \n" + plan + "\n include this [Content]. Disregard all information about guardrail intervention. + \n" + full_text
+            if (DEBUG):
+                print("Prompt: " + final_query)
+            final_report = agent(final_query)
+            if USE_GUARDRAILS.lower() == "true" :
+                print(guardrail_check(str(final_report)))
+            else:
+                print(str(final_report))
+        
+
+            
