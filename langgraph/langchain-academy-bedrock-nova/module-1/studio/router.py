a364976a069119eac7102c2e0efd50b09c538448
Gopi Krishnamurthy <gopikwork@gmail.com>
2025-08-21 16:32:40 +0000

    adding langgraph course targeting Amazon Nova models

---
+++ langgraph/langchain-academy-bedrock-nova/module-1/studio/router.py
@@ -0,0 +1,38 @@
+from langchain_aws import ChatBedrock
+from langgraph.graph import MessagesState
+from langgraph.graph import StateGraph, START, END
+from langgraph.prebuilt import ToolNode, tools_condition
+
+# Tool
+def multiply(a: int, b: int) -> int:
+    """Multiplies a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a * b
+
+# LLM with bound tool
+llm =  ChatBedrock(model_id="us.amazon.nova-pro-v1:0", temperature=0)
+llm_with_tools = llm.bind_tools([multiply])
+
+# Node
+def tool_calling_llm(state: MessagesState):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+# Build graph
+builder = StateGraph(MessagesState)
+builder.add_node("tool_calling_llm", tool_calling_llm)
+builder.add_node("tools", ToolNode([multiply]))
+builder.add_edge(START, "tool_calling_llm")
+builder.add_conditional_edges(
+    "tool_calling_llm",
+    # If the latest message (result) from assistant is a tool call -> tools_condition routes to tools
+    # If the latest message (result) from assistant is a not a tool call -> tools_condition routes to END
+    tools_condition,
+)
+builder.add_edge("tools", END)
+
+# Compile graph
+graph = builder.compile()
